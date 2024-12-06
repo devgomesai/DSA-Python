a2028b3750bf30f774cd35df9945dbb1c150fafc
JonathanJourney99 <ijonathan.gome@gmail.com>
2024-12-06 11:24:44 +0530

    Only keeping the Functions

--- Leetcode-Python/Linked-List/link-list.py
+++ Leetcode-Python/Linked-List/link-list.py
@@ -10,6 +10,25 @@ class LinkedList:
         self.head = self.new_node # make the head ponit to the node with a value
         self.tail = self.new_node # also the tail as only one node
         self.length = 1 # as only one node so length is 1
+
+    # Insert a Node
+    def insert_node(self, index, value):
+
+        if index < 0 or index > self.length:
+            return False
+
+        if index == 0:
+            return self.prepend(value=value)
+
+        if index == self.length:
+            return self.Append(value=value)
+
+        node = Node(value=value)
+        temp = self.get(index=index-1) # node before main index i.e if index = 2 then prev_node is node at 1
+        node.next = temp.next
+        temp.next = node
+        self.length += 1
+        return True
         
     # Append Method
     def Append(self, value): # Only required is the ****tail****
@@ -42,11 +61,28 @@ class LinkedList:
             self.tail = pre # when temp.next is None breaked the loop and make tail as prev node
             self.tail.next = None # and make prev node next as none
             self.length -= 1 # lenght of the list decrement
-            if self.length == 0: # if no node in LL
+            if self.length == 0: # if no node in LL  ****self.length -= 1****
                 self.head = None
                 self.tail = None
             return temp 
+
+    # Pop First Node
+    def pop_first_node(self):
+        if self.length == 0:
+            return None
+
+        temp = self.head # start of LL let temp also be
+        self.head = self.head.next # case: None so head becomes none 
+        temp.next = None
+        self.length -= 1 
+
+        if self.length == 0: # if no node in LL this is for ****self.length -= 1**** when turns to zero
+            # self.head = None not required as happens on line 54
+            self.tail = None
+    
+        return temp
         
+    # Add Node to the Begining
     def prepend(self, value): # add node in the begining so ****head****
         new_node = Node(value)
         if self.length == 0:
@@ -57,34 +93,58 @@ class LinkedList:
             self.head = new_node
         self.length += 1
 
+    # get a node via index
+    def get(self, index):
+        # Check if the given index is valid
+        if index < 0 or index >= self.length:
+            return None
+        # assign head to temp
+        temp = self.head
+        # for loop to move in the LL and get to the index node
+        for _ in range(index):
+            temp = temp.next
+        return temp # once reached the node return it
 
-# Create a Linked List with a node creation
-print('*' * 13 +'Creating a Linked List'+ '*' * 13)
-LinkedList_1 = LinkedList(1)
-# Append a Node with a node creation
-print('*' * 13 +'Appending in  a Linked List'+ '*' * 13)
-LinkedList_1.Append(2)
-LinkedList_1.Append(3)
-LinkedList_1.Append(4)
-LinkedList_1.Append(5)
-LinkedList_1.Append(6)
-# Display
-print('*' * 13 +'Display'+ '*' * 13)
-LinkedList_1.Display()
-#Pop
-print('*' * 13 +'Pop'+ '*' * 13)
-poped_element = LinkedList_1.Pop()
-# Display
-print('*' * 13 +'Display'+ '*' * 13)
-LinkedList_1.Display()
+    # Set a node value at a given index
+    def set(self, index, value):
+        # call get() method to get the node
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
 
-print("The Poped Element is :",poped_element.value)
+    # remove a node
+    def remove(self, index):
 
-# Add a node in front
-node = 10
-print(('*' * 13 +'Adding a Node in Front: {}'+ '*' * 13).format(node))
-LinkedList_1.prepend(node)
+        if index < 0 or index >=self.length:
+            return None
 
-# Display
-print('*' * 13 +'Display'+ '*' * 13)
-LinkedList_1.Display()
+        if index == 0:
+            return self.pop_first_node()
+
+        if index == self.length - 1:
+            return self.Pop()
+        # middle node
+        prev_node = self.get(index=index-1)
+        rm_node = prev_node.next
+        prev_node.next = rm_node.next
+        rm_node.next = None
+        self.length -= 1
+        return rm_node
+
+    # Reverse a Linked List
+    def reverse(self):
+
+        temp = self.head  # set temp with head
+        self.head = self.tail # set head with tail
+        self.tail = temp  # set tail with head
+
+        after = temp.next # create 2 variables
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
