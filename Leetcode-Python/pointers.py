3dfc6e223e5f6a055e66a82c62893a6836e844b3
JonathanJourney99 <ijonathan.gomes@gmail>
2025-03-14 12:19:47 +0530

    Updated pointers.py

--- Leetcode-Python/pointers.py
+++ Leetcode-Python/pointers.py
@@ -2,15 +2,15 @@ num1 = 11
 
 num2 = num1
 
-print('Before num2 value is updated')
-print("num1= ", num1)
-print("num2= ", num2)
+print('Before num2 value is updated',end="\n")
+print("num1 = ", num1)
+print("num2 = ", num2)
 
 print("\nnum1 points to = ", id(num1))
 print("num2 points to =  ", id(num2))
-
+print("\n")
 num2 = 22
-print('After num2 value is updated')
+print('After num2 value is updated', end="\n")
 print("num1= ", num1)
 print("num2= ", num2)
 
