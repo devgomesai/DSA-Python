209c70cafebbf13bd2215f7659a3064608ce1fb8
JonathanJourney99 <ijonathan.gomes@gmail>
2025-07-02 21:15:56 +0530

    heaps

--- Leetcode-Python/Heaps/heaps.py
+++ Leetcode-Python/Heaps/heaps.py
@@ -40,42 +40,66 @@ class MaxHeap:
             self._swap(current, self._parent(current))                
             current = self._parent(current)
             
-    def get_right_child(self, value):
-        try:
-            _index = self.heap.index(value)
-            _right_index = self._right_child(_index)
-            if _right_index < len(self.heap):
-                print(f"Right Child of {value}: {self.heap[_right_index]}")
-        except ValueError:
-            print(f"Value {value} not found in heap.")
+    def _sink_down(self, index):
+        max_index = index # get the head index i.e 0
+        while True:
+            left_index = self._left_child(index=index)
+            right_index = self._right_child(index=index)
+            
+            if (left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+                
+            if (right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+                
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
         
-    def get_left_child(self,value):
-        try:
-            _index = self.heap.index(value)
-            _left_index = self._left_child(_index)
-            if _left_index < len(self.heap):
-                print(f"Left Child of {value}: {self.heap[_left_index]}")
-        except ValueError:
-            print(f"Value {value} not found in heap.")
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        max_value = self.heap[0] # store first item into max_value variable
+        self.heap[0] = self.heap.pop() # set the index 0 to the last value of the heap  
+        self._sink_down(0) # sink the top value based on prev code to sort the max-heap
+        
+        return max_value
+            
+    # def get_right_child(self, value):
+    #     try:
+    #         _index = self.heap.index(value)
+    #         _right_index = self._right_child(_index)
+    #         if _right_index < len(self.heap):
+    #             print(f"Right Child of {value}: {self.heap[_right_index]}")
+    #     except ValueError:
+    #         print(f"Value {value} not found in heap.")
+        
+    # def get_left_child(self,value):
+    #     try:
+    #         _index = self.heap.index(value)
+    #         _left_index = self._left_child(_index)
+    #         if _left_index < len(self.heap):
+    #             print(f"Left Child of {value}: {self.heap[_left_index]}")
+    #     except ValueError:
+    #         print(f"Value {value} not found in heap.")
     
     
     
 max_heap = MaxHeap()
-max_heap.insert(99)
-max_heap.insert(72)
-max_heap.insert(61)
-max_heap.insert(58)
-
-print(max_heap.heap)
-
-max_heap.insert(100)
-print('-'*16)
-print(max_heap.heap)
-print('-'*16)
+max_heap.insert(95)
 max_heap.insert(75)
+max_heap.insert(80)
+max_heap.insert(55)
+max_heap.insert(60)
+max_heap.insert(50)
+max_heap.insert(65)
+
 print(max_heap.heap)
 
-max_heap.get_right_child(100)
-max_heap.get_left_child(100)
+max_heap.remove()
 
-                
+print(max_heap.heap)
